a34796e3b44b3bffdee68ed950cb5b09cf8c53d5
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-14 13:14:43 +0500

    feat: подключение к mongo из JS

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/newsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 const { PORT = 3000 } = process.env;
 
