6a69b233dbf56846d2c0e071008e2d0a1a10bdd8
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-14 16:00:07 +0500

    feat: register and login user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 module.exports.getInfoUser = (req, res, next) => {
   const { email } = req.body;
@@ -7,5 +8,30 @@ module.exports.getInfoUser = (req, res, next) => {
       //throw new Error
     }
     return res.status(200).send(user);
+  }).catch(next);
+};
+
+module.exports.registerUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+  User.findOne({ email }).then((findEmail) => {
+    if (findEmail) {
+      //такой имейл уже есть
+      return console.log('такой имейл уже есть');
+    }
+    return bcrypt.hash(password, 10).then((hash) => {
+      User.create({ email, password: hash, name }).then((user) =>
+      res.status(201).send(user)).catch(next)
+    });
+  }).catch(next);
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password).then((token) => {
+    res.status(200).cookie('jwt', token, {
+      maxAge: 3600000 * 24,
+      httpOnly: true,
+      sameSite: true,
+    }).end();
   }).catch(next);
 };
