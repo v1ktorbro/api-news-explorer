f3da860b73c1b463e7c78b82036d0a00f6ae18c9
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-14 14:41:24 +0500

    feat: метод findUserByCredentials

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,5 +20,25 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 }, { versionKey: false });
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password').then((user) => {
+    if (!user) {
+      //throw new Error
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        //throw new
+      }
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '1d' },
+      );
+      return token;
+    });
+  });
+};
 
 module.exports = mongoose.model('user', userSchema);
