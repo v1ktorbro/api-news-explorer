74e3f5b0d29bc6a139952baa1ac0d63c0d40f099
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-14 12:54:48 +0500

    feat: routes for articles

---
+++ routes/articles.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { getArticlesUser, createArticle, deleteArticle } = require('../controllers/articles');
+
+router.get('/', getArticlesUser);
+router.post('/', createArticle);
+router.delete('/:id', deleteArticle);
