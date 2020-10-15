59cdcf2c36fc4550ce184ad1448fc89998ad9f38
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-15 14:15:05 +0500

    fix: применить конструкторы ошибок в контроллерах

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const { getCurrentUserId } = require('../middlewares/auth');
+const { NotFound, EmailError } = require('../errors/index');
 
 module.exports.getInfoUser = (req, res, next) => {
   User.findById(getCurrentUserId(req)).then((user) => {
     if (!user) {
-      //throw new Error
+      throw new NotFound('Неправильно передан id пользователя');
     }
     return res.status(200).send(user);
   }).catch(next);
@@ -16,9 +17,7 @@ module.exports.registerUser = (req, res, next) => {
   const email = req.body.email.toLowerCase();
   User.findOne({ email }).then((findEmail) => {
     if (findEmail) {
-      //такой имейл уже есть
-      res.status(499).send('tакой имейл уже есть')
-      return console.log('такой имейл уже есть');
+      throw new EmailError(`${findEmail} уже зарегистрирован`);
     }
     return bcrypt.hash(password, 10).then((hash) => {
       User.create({ email, password: hash, name }).then((user) => {
