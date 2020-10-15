2adeee96a4614fada1194864e5a1259accdf775f
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-15 17:10:15 +0500

    fix: выдавать оишбку неавторизированным пользователям

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
+const { Unauthorized } = require('../errors/index');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.authorization = (req, res, next) => {
   const payload = req.cookies.jwt;
   if (!payload) {
-    //throw new Unauhorized
-    return console.log('АВТОРИЗИРУЙСЯ, ХОЛОП!')
+    throw new Unauthorized('Для продолжения необходимо авторизироваться');
   }
   return next();
 };
 
+// eslint-disable-next-line no-unused-vars
 module.exports.getCurrentUserId = (req, res, next) => jwt.verify(req.cookies.jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret')._id;
